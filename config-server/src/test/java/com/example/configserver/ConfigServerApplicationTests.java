eea88e1775dbdb0cb207daf7fea2d2c9aafd7283
a-aivazian <artguruaivazian@gmail.com>
2023-06-27 15:38:57 +0500

    config-server

--- config-server/src/test/java/com/example/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/example/configserver/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.userservice;
+package com.example.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class ConfigServerApplicationTests {
 
     @Test
     void contextLoads() {
